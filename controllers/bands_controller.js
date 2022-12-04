10e52d308f6f346499462ab17eef0454186f92e8
Alex Schmidt <alex.schmidt767@gmail.com>
2022-12-04 00:30:41 -0600

    get route set up to get a specific band, post route for making new band set up but not tested

--- controllers/bands_controller.js
+++ controllers/bands_controller.js
@@ -7,7 +7,34 @@ const { Band } = db
 bands.get('/', async (req, res) => {
     try {
         const foundBands = await Band.findAll()
+        console.log('found all bands')
         res.status(200).json(foundBands)
+    } catch (error) {
+        console.log('error')
+        res.status(500).json(error)
+    }
+})
+
+// FIND A SPECIFIC BAND
+bands.get('/:id', async (req, res) => {
+    try {
+        const foundBand = await Band.findOne({
+            where: { band_id: req.params.id }
+        })
+        res.status(200).json(foundBand)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+// CREATE A BAND
+bands.post('/', async (req, res) => {
+    try {
+        const newBand = await Band.create(req.body)
+        res.status(200).json({
+            message: 'Succsessfully inserted a new band',
+            data: newBand
+        })
     } catch (error) {
         res.status(500).json(error)
     }
