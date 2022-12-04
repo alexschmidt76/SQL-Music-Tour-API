1be1f29a1528d71ed92e467b67e8d6e7ec830149
Alex Schmidt <alex.schmidt767@gmail.com>
2022-12-04 00:46:21 -0600

    delete route for bands set up

--- controllers/bands_controller.js
+++ controllers/bands_controller.js
@@ -44,12 +44,24 @@ bands.post('/', async (req, res) => {
 bands.put('/:id', async (req, res) => {
     try {
         const updatedBands = await Band.update(req.body, {
-            where: {
-                band_id: req.params.id
-            }
+            where: { band_id: req.params.id }
         })
         res.status(200).json({
             message: `Successfully updated ${updatedBands} band(s)`
+        })
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+// DELETE A BAND
+bands.delete('/:id', async (req, res) => {
+    try {
+        const deletedBands = await Band.destroy({
+            where: { band_id: req.params.id }
+        })
+        res.status(200).json({
+            message: `Succsessfully deleted ${deletedBands} band(s)`
         })
     } catch (error) {
         res.status(500).json(error)
